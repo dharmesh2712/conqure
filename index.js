4ca3f9f49e00a37cd84d80f01ed1cfb00f3a21bd
dharmesh2712 <85621362+dharmesh2712@users.noreply.github.com>
2021-11-24 19:20:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+nav = document.querySelector('.nav')
+nav_left  = document.querySelector('.nav_left')
+nav_right = document.querySelector('.nav_right')
+
+burger.addEventListener('click', ()=>{
+
+    nav_right.classList.toggle('v-nav-resp');
+    nav_left.classList.toggle('v-nav-resp');
+    nav.classList.toggle('h-nav-resp');
+    
+})
